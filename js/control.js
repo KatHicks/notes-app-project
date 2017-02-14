d7eb2542d1bb648bf28d2d7dbfe9538a9180e98f
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-5.local>
2017-02-14 17:19:01 +0000

    Now we can save notes, and display list of notes below.

--- js/control.js
+++ js/control.js
@@ -1,8 +1,11 @@
 var control = (function () {
   var newNoteButton = document.getElementById("show-new-note-button");
+
   var saveNewNote = document.getElementById("save-new-note");
+  var noteText = document.getElementById("note-text");
   var submitNewNoteForm = document.getElementById("submit-new-note-form");
 
+  var notesList = new List();
 
   var init = function () {
     newNoteButton.addEventListener("click", function() {
@@ -11,10 +14,20 @@ var control = (function () {
       event.preventDefault();
     });
     saveNewNote.addEventListener("click", function() {
+      var note = new Note(noteText.value);
+      notesList.addNotes(note);
+      refreshList();
       submitNewNoteForm.style.display = "none";
       newNoteButton.style.display = "block";
       event.preventDefault();
     });
+  };
+
+  var refreshList = function () {
+    previews.innerHTML = "";
+    for (var i = 0; i < notesList.getNotes().length; i++) {
+      previews.insertAdjacentHTML("beforeend", ("<li>"+((notesList.getNotes())[i]).getPreview())+"</li>")
+    };
   };
 
   return {
